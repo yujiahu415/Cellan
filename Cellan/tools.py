95c62871d52286a11d84f68c0e3d3f41b70fc28a
Yujia Hu <yujiahu415@gmail.com>
2024-11-04 13:38:35 -0500

    Update tools.py

--- Cellan/tools.py
+++ Cellan/tools.py
@@ -19,8 +19,8 @@ def extract_images(path_to_file,out_folder,fov_div):
 	c_list=[i for i in file.get_iter_c(t=0,z=0)]
 
 	image=np.array(file.get_frame(z=0,t=0,c=0))
-	fov_width=int(image.shape[0]/fov_div)
-	fov_height=int(image.shape[1]/fov_div)
+	fov_width=int(image.shape[1]/fov_div)
+	fov_height=int(image.shape[0]/fov_div)
 
 	for w in range(fov_div):
 
@@ -29,5 +29,5 @@ def extract_images(path_to_file,out_folder,fov_div):
 			for c in range(len(c_list)):
 
 				image=np.array(file.get_frame(z=0,t=0,c=c))
-				fov=np.uint8(exposure.rescale_intensity(image[w*fov_width:(w+1)*fov_width,h*fov_height:(h+1)*fov_height],out_range=(0,255)))
+				fov=np.uint8(exposure.rescale_intensity(image[h*fov_height:(h+1)*fov_height,w*fov_width:(w+1)*fov_width],out_range=(0,255)))
 				cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0],'_'+str(w)+str(h)+'_c'+str(c)+'.png'),fov)
