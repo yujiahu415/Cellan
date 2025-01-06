6573d5dba9459aeaa6bdb6b369ceafab9b5d8997
Yujia Hu <yujiahu415@gmail.com>
2025-01-06 13:24:13 -0500

    Update tools.py

--- Cellan/tools.py
+++ Cellan/tools.py
@@ -9,25 +9,37 @@ from skimage import exposure
 
 def extract_images(path_to_file,out_folder,fov_div):
 
-	if os.path.splitext(os.path.basename(path_to_file))[1] in ['.tif','.TIF','.tiff','.TIFF']:
-		tifdata=imread(path_to_file)
-		file=[i for i in tifdata]
+	if os.path.splitext(os.path.basename(path_to_file))[1] in ['.tif','.TIF','.tiff','.TIFF','.svs','.SVS']:
+
+		image=imread(path_to_file)
+
+		fov_width=int(image.shape[1]/fov_div)
+		fov_height=int(image.shape[0]/fov_div)
+
+		for w in range(fov_div):
+
+			for h in range(fov_div):
+
+				fov=np.uint8(exposure.rescale_intensity(image[h*fov_height:(h+1)*fov_height,w*fov_width:(w+1)*fov_width],out_range=(0,255)))
+				cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'.png'),fov)
+
 	else:
+
 		lifdata=LifFile(path_to_file)
 		file=[i for i in lifdata.get_iter_image()][0]
 
-	c_list=[i for i in file.get_iter_c(t=0,z=0)]
+		c_list=[i for i in file.get_iter_c(t=0,z=0)]
 
-	image=np.array(file.get_frame(z=0,t=0,c=0))
-	fov_width=int(image.shape[1]/fov_div)
-	fov_height=int(image.shape[0]/fov_div)
+		image=np.array(file.get_frame(z=0,t=0,c=0))
+		fov_width=int(image.shape[1]/fov_div)
+		fov_height=int(image.shape[0]/fov_div)
 
-	for w in range(fov_div):
+		for w in range(fov_div):
 
-		for h in range(fov_div):
+			for h in range(fov_div):
 
-			for c in range(len(c_list)):
+				for c in range(len(c_list)):
 
-				image=np.array(file.get_frame(z=0,t=0,c=c))
-				fov=np.uint8(exposure.rescale_intensity(image[h*fov_height:(h+1)*fov_height,w*fov_width:(w+1)*fov_width],out_range=(0,255)))
-				cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'_c'+str(c)+'.png'),fov)
+					image=np.array(file.get_frame(z=0,t=0,c=c))
+					fov=np.uint8(exposure.rescale_intensity(image[h*fov_height:(h+1)*fov_height,w*fov_width:(w+1)*fov_width],out_range=(0,255)))
+					cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'_c'+str(c)+'.png'),fov)
