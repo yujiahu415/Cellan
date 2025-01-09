4ce590ae1ef8c644876eb50807169e5777216290
Yujia Hu <yujiahu415@gmail.com>
2025-01-09 17:09:25 -0500

    Update tools.py

--- Cellan/tools.py
+++ Cellan/tools.py
@@ -32,18 +32,26 @@ def extract_images(path_to_file,out_folder,fov_div,imagewidth=None):
 		fov_width=int(image.shape[1]/fov_div)
 		fov_height=int(image.shape[0]/fov_div)
 
-		c_list=[0,1,2]
+		if len(list(image.shape))<3:
+			c_list=None
+		else:
+			c_list=[0,1,2]
 
 		for w in range(fov_div):
 
 			for h in range(fov_div):
 
-				for c in range(len(c_list)):
-
-					fov=np.uint8(exposure.rescale_intensity(image[h*fov_height:(h+1)*fov_height,w*fov_width:(w+1)*fov_width,c],out_range=(0,255)))
+				if c_list is None:
+					fov=np.uint8(exposure.rescale_intensity(image[h*fov_height:(h+1)*fov_height,w*fov_width:(w+1)*fov_width],out_range=(0,255)))
 					if imagewidth is not None:
 						fov=cv2.resize(fov,(imagewidth,int(fov.shape[0]*imagewidth/fov.shape[1])),interpolation=cv2.INTER_AREA)
-					cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'_c'+str(c)+'.png'),fov)
+					cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'.png'),fov)
+				else:
+					for c in range(len(c_list)):
+						fov=np.uint8(exposure.rescale_intensity(image[h*fov_height:(h+1)*fov_height,w*fov_width:(w+1)*fov_width,c],out_range=(0,255)))
+						if imagewidth is not None:
+							fov=cv2.resize(fov,(imagewidth,int(fov.shape[0]*imagewidth/fov.shape[1])),interpolation=cv2.INTER_AREA)
+						cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'_c'+str(c)+'.png'),fov)
 
 	else:
 
