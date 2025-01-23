abc126281868d604548c00d8519ba9c0d28f1fff
Yujia Hu <yujiahu415@gmail.com>
2025-01-23 13:02:49 -0500

    Update tools.py

--- Cellan/tools.py
+++ Cellan/tools.py
@@ -18,7 +18,6 @@ def extract_images(path_to_file,out_folder,fov_dim,black_background=True):
 		num_w=int(width/fov_dim)
 		num_h=int(height/fov_dim)
 
-
 		for h in range(num_h+1):
 
 			for w in range(num_w+1):
@@ -31,7 +30,7 @@ def extract_images(path_to_file,out_folder,fov_dim,black_background=True):
 						background=np.uint8(np.ones((fov_dim,fov_dim,3),dtype='uint8')*255)
 					background[:fov.shape[0],:fov.shape[1]]=fov
 					fov=background
-				cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'.jpg'),fov)
+				imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'.jpg'),fov)
 
 	elif os.path.splitext(os.path.basename(path_to_file))[1] in ['.tif','.TIF','.tiff','.TIFF']:
 
@@ -45,34 +44,32 @@ def extract_images(path_to_file,out_folder,fov_dim,black_background=True):
 			c_list=[0,1,2]
 		num_w=int(width/fov_dim)
 		num_h=int(height/fov_dim)
-		if black_background:
-			if c_list is None:
-				background=np.zeros((fov_dim,fov_dim),dtype='uint8')
-			else:
-				background=np.zeros((fov_dim,fov_dim,3),dtype='uint8')
-		else:
-			if c_list is None:
-				background=np.uint8(np.ones((fov_dim,fov_dim),dtype='uint8')*255)
-			else:
-				background=np.uint8(np.ones((fov_dim,fov_dim,3),dtype='uint8')*255)
 
-		for h in range(num_h):
+		for h in range(num_h+1):
 
-			for w in range(num_w):
+			for w in range(num_w+1):
 
 				if c_list is None:
 					fov=np.uint8(exposure.rescale_intensity(image[int(h*fov_dim):min(int((h+1)*fov_dim),height),int(w*fov_dim):min(int((w+1)*fov_dim),width)],out_range=(0,255)))
 					if fov.shape[0]<fov_dim or fov.shape[1]<fov_dim:
-						background[fov]=fov
+						if black_background:
+							background=np.zeros((fov_dim,fov_dim),dtype='uint8')
+						else:
+							background=np.uint8(np.ones((fov_dim,fov_dim),dtype='uint8')*255)
+						background[:fov.shape[0],:fov.shape[1]]=fov
 						fov=background
-					cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'.jpg'),fov)
+					imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'.jpg'),fov)
 				else:
 					for c in c_list:
 						fov=np.uint8(exposure.rescale_intensity(image[int(h*fov_dim):min(int((h+1)*fov_dim),height),int(w*fov_dim):min(int((w+1)*fov_dim),width),c],out_range=(0,255)))
 						if fov.shape[0]<fov_dim or fov.shape[1]<fov_dim:
-							background[fov]=fov
+							if black_background:
+								background=np.zeros((fov_dim,fov_dim,3),dtype='uint8')
+							else:
+								background=np.uint8(np.ones((fov_dim,fov_dim,3),dtype='uint8')*255)
+							background[:fov.shape[0],:fov.shape[1]]=fov
 							fov=background
-						cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'_c'+str(c)+'.jpg'),fov)
+						imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'_c'+str(c)+'.jpg'),fov)
 
 	elif os.path.splitext(os.path.basename(path_to_file))[1] in ['.qptiff','.QPTIFF']:
 
@@ -83,20 +80,20 @@ def extract_images(path_to_file,out_folder,fov_dim,black_background=True):
 		height=image.shape[1]
 		num_w=int(width/fov_dim)
 		num_h=int(height/fov_dim)
-		if black_background:
-			background=np.zeros((fov_dim,fov_dim),dtype='uint8')
-		else:
-			background=np.uint8(np.ones((fov_dim,fov_dim),dtype='uint8')*255)
 
 		for c in c_list:
 
-			for w in range(fov_dim):
+			for h in range(num_h+1):
 
-				for h in range(fov_dim):
+				for w in range(num_w+1):
 
 					fov=np.uint8(exposure.rescale_intensity(image[c,int(h*fov_dim):min(int((h+1)*fov_dim),height),int(w*fov_dim):min(int((w+1)*fov_dim),width)],out_range=(0,255)))
 					if fov.shape[0]<fov_dim or fov.shape[1]<fov_dim:
-						background[fov]=fov
+						if black_background:
+							background=np.zeros((fov_dim,fov_dim),dtype='uint8')
+						else:
+							background=np.uint8(np.ones((fov_dim,fov_dim),dtype='uint8')*255)
+						background[:fov.shape[0],:fov.shape[1]]=fov
 						fov=background
 					imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'_c'+str(c)+'.jpg'),fov)
 
@@ -112,22 +109,22 @@ def extract_images(path_to_file,out_folder,fov_dim,black_background=True):
 		height=image.shape[0]
 		num_w=int(width/fov_dim)
 		num_h=int(height/fov_dim)
-		if black_background:
-			background=np.zeros((fov_dim,fov_dim),dtype='uint8')
-		else:
-			background=np.uint8(np.ones((fov_dim,fov_dim),dtype='uint8')*255)
 
-		for h in range(num_h):
+		for h in range(num_h+1):
 
-			for w in range(num_w):
+			for w in range(num_w+1):
 
 				for c in range(len(c_list)):
 
 					image=np.array(file.get_frame(z=0,t=0,c=c))
 					fov=np.uint8(exposure.rescale_intensity(image[int(h*fov_dim):min(int((h+1)*fov_dim),height),int(w*fov_dim):min(int((w+1)*fov_dim),width)],out_range=(0,255)))
 					if fov.shape[0]<fov_dim or fov.shape[1]<fov_dim:
-						background[fov]=fov
+						if black_background:
+							background=np.zeros((fov_dim,fov_dim),dtype='uint8')
+						else:
+							background=np.uint8(np.ones((fov_dim,fov_dim),dtype='uint8')*255)
+						background[:fov.shape[0],:fov.shape[1]]=fov
 						fov=background
-					cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'_c'+str(c)+'.jpg'),fov)
+					imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'_c'+str(c)+'.jpg'),fov)
 
 
