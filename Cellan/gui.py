4a048e3653da73da9fb63a672af4bdf812ffcc35
Yujia Hu <yujiahu415@gmail.com>
2025-01-23 18:00:13 -0500

    Update gui.py

--- Cellan/gui.py
+++ Cellan/gui.py
@@ -880,7 +880,7 @@ class WindowLv2_AnalyzeMultiChannels(wx.Frame):
 		else:
 
 			for i in self.path_to_files:
-				AC=AnalyzeCells(i,self.result_path,self.path_to_detector,self.cell_kinds,detection_threshold=self.detection_threshold,expansion=self.expansion,fov_dim=self.fov_dim,black_background=self.black_background)
+				AC=AnalyzeCells(i,self.result_path,self.path_to_detector,self.cell_kinds,detection_threshold=self.detection_threshold,expansion=self.expansion)
 				AC.analyze_multichannels(self.names_colors,detection_channel=self.detection_channel,analysis_channels=self.analysis_channels)
 
 
@@ -1068,7 +1068,7 @@ class WindowLv2_AnalyzeSingleChannel(wx.Frame):
 		else:
 
 			for i in self.path_to_files:
-				AC=AnalyzeCells(i,self.result_path,self.path_to_detector,self.cell_kinds,detection_threshold=self.detection_threshold,expansion=self.expansion,fov_dim=self.fov_dim,black_background=self.black_background)
+				AC=AnalyzeCells(i,self.result_path,self.path_to_detector,self.cell_kinds,detection_threshold=self.detection_threshold,expansion=self.expansion)
 				AC.analyze_singlechannel(self.names_colors)
 
 
