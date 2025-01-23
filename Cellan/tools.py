ee680b0018ed3b2b1d0e186437efb700dc5a08a7
Yujia Hu <yujiahu415@gmail.com>
2025-01-23 12:51:25 -0500

    Update tools.py

--- Cellan/tools.py
+++ Cellan/tools.py
@@ -17,10 +17,7 @@ def extract_images(path_to_file,out_folder,fov_dim,black_background=True):
 		height=image.shape[0]
 		num_w=int(width/fov_dim)
 		num_h=int(height/fov_dim)
-		if black_background:
-			background=np.zeros((fov_dim,fov_dim,3),dtype='uint8')
-		else:
-			background=np.uint8(np.ones((fov_dim,fov_dim,3),dtype='uint8')*255)
+
 
 		for h in range(num_h+1):
 
@@ -28,6 +25,10 @@ def extract_images(path_to_file,out_folder,fov_dim,black_background=True):
 
 				fov=np.uint8(exposure.rescale_intensity(image[int(h*fov_dim):min(int((h+1)*fov_dim),height),int(w*fov_dim):min(int((w+1)*fov_dim),width)],out_range=(0,255)))
 				if fov.shape[0]<fov_dim or fov.shape[1]<fov_dim:
+					if black_background:
+						background=np.zeros((fov_dim,fov_dim,3),dtype='uint8')
+					else:
+						background=np.uint8(np.ones((fov_dim,fov_dim,3),dtype='uint8')*255)
 					background[:fov.shape[0],:fov.shape[1]]=fov
 					fov=background
 				cv2.imwrite(os.path.join(out_folder,os.path.splitext(os.path.basename(path_to_file))[0]+'_'+str(w)+str(h)+'.jpg'),fov)
