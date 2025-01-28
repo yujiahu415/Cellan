1391eb3b3a88b41691d205cb04cf237d41cb396b
Yujia Hu <yujiahu415@gmail.com>
2025-01-28 14:53:31 -0500

    Update analyzer.py

--- Cellan/analyzer.py
+++ Cellan/analyzer.py
@@ -45,16 +45,16 @@ class AnalyzeCells():
 			detect_image=np.array(file.get_frame(z=0,t=0,c=detection_channel))
 			if len(analysis_channels)==0:
 				c_list=[i for i in file.get_iter_c(t=0,z=0)]
-				analysis_channels=[c for c in range(c_list)]
+				analysis_channels=c_list
 		else:
 			if os.path.splitext(os.path.basename(self.path_to_file))[1] in ['.qptiff','.QPTIFF']:
 				detect_image=imread(self.path_to_file)[detection_channel,:,:]
 				if len(analysis_channels)==0:
-					analysis_channels=[i for i in range(imread(self.path_to_file).shape[0])]
+					analysis_channels=list(range(imread(self.path_to_file).shape[0]))
 			else:
 				detect_image=imread(self.path_to_file)[:,:,detection_channel]
 				if len(analysis_channels)==0:
-					analysis_channels=[0,1,2]
+					analysis_channels=list(range(imread(self.path_to_file).shape[2]))
 
 		cell_numbers={}
 		cell_centers={}
