cf002b0a19ed2e92f076b62529fb61f365cbf9d0
Yujia Hu <yujiahu415@gmail.com>
2025-02-11 16:56:20 -0500

    Update tools.py

--- Cellan/tools.py
+++ Cellan/tools.py
@@ -222,7 +222,7 @@ def calculate_totalintensity(path_to_file,results_path):
 		dfs['area_'+str(c)]=areas[c]
 		dfs['intensity_'+str(c)]=intensities[c]
 	dfs=pd.DataFrame(dfs,index=['value'])
-	out_sheet=os.path.join(results_path,os.path.splitext(os.path.basename(path_to_file))[0]+'_total_intensity.xlsx')
+	out_sheet=os.path.join(results_path,os.path.splitext(os.path.basename(path_to_file))[0],os.path.splitext(os.path.basename(path_to_file))[0]+'_total_intensity.xlsx')
 	dfs.to_excel(out_sheet,float_format='%.2f')
 
 	print('Analysis completed!')
