56462445e32dfe61242e0ae0e4aecaec8009b144
Yujia Hu <yujiahu415@gmail.com>
2025-03-07 15:43:24 -0500

    Update gui.py

--- Cellan/gui.py
+++ Cellan/gui.py
@@ -1450,7 +1450,7 @@ class WindowLv2_AnalyzeMultiChannels(wx.Frame):
 
 	def __init__(self,title):
 
-		super(WindowLv2_AnalyzeMultiChannels,self).__init__(parent=None,title=title,size=(1000,370))
+		super(WindowLv2_AnalyzeMultiChannels,self).__init__(parent=None,title=title,size=(1000,390))
 		self.detector_path=None
 		self.path_to_detector=None
 		self.cell_kinds=None
@@ -1712,7 +1712,7 @@ class WindowLv2_AnalyzeSingleChannel(wx.Frame):
 
 	def __init__(self,title):
 
-		super(WindowLv2_AnalyzeSingleChannel,self).__init__(parent=None,title=title,size=(1000,320))
+		super(WindowLv2_AnalyzeSingleChannel,self).__init__(parent=None,title=title,size=(1000,340))
 		self.detector_path=None
 		self.path_to_detector=None
 		self.cell_kinds=None
